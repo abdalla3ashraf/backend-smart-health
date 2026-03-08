ba801b80846231faaf5b11d0d3910fcbbf04d261
abdalla <abdalla3ashraf@gmail.com>
2026-03-08 02:09:15 +0200

    fix versal server

--- api/index.js
+++ api/index.js
@@ -15,7 +15,8 @@
 
 // app.listen(4000, () => console.log(" Server running on PORT 4000"));
 
-import app from "./src/app.js";
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => {
-    console.log(` Server running on PORT ${PORT}`)});
+import app from "../src/app.js";
+// const PORT = process.env.PORT || 4000
+// app.listen(PORT, () => {
+//     console.log(` Server running on PORT ${PORT}`)});
+export default app;
