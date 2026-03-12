f4324c1f4458f56c176daeafd128500eb5e94d39
abdalla <abdalla3ashraf@gmail.com>
2026-03-12 04:39:01 +0200

    d

--- src/app.js
+++ src/app.js
@@ -19,18 +19,28 @@ const app = express()
 // })) 
 // app.js
 
-
 // ...
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:5502"], // في مابعد حدد دومين الويب ودومين الـ API
+  origin: "*", // في مابعد حدد دومين الويب ودومين الـ API
   credentials: true
 }));
 
 // لازم الترتيب ده:
 app.use(express.json());
+app.use(session({
+    secret:"supersecretkey",
+    resave: false,
+    saveUninitialized: false
+}))
 app.use(passport.initialize()); 
-// ابعد عن passport.session() لو هتشتغل JWT (وده الأفضل لمشروعكم)
-app.use(express.json())
+app.use(passport.session())
+
+
+app.use("/auth",authRoutes)
+app.use("/user",userRoutes)
+app.use("/patient",patientRoutes)
+app.use("/api/patientinfo",patientinfoRoutes)
+app.use("/api/patient",patientinfoRoutes)
 
 app.get("/test",(req,res)=>{
     console.log("server work");
@@ -40,18 +50,10 @@ app.get("/test",(req,res)=>{
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-app.use("/patient",patientRoutes)
-app.use("/api/patientinfo",patientinfoRoutes)
-app.use("/api/patient",patientinfoRoutes)
-app.use(session({
-    secret:"supersecretkey",
-    resave: false,
-    saveUninitialized: false
-}))
+
+
 app.use("/home",homeRoutes)
-app.use(passport.initialize())
+
 //console.log(passport._strategies);
-app.use(passport.session())
-app.use("/auth",authRoutes)
-app.use("/user",userRoutes)
+
 export default app;
